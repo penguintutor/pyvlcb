3d1b94672360119e53016d0cd968c479f2e038e1
Stewart Watkiss <penguintutor@users.noreply.github.com>
2025-09-26 19:14:07 +0100

    Add support for QFileDialog to dialog window focus

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 import sys
-from PySide6.QtWidgets import QApplication, QDialog
+from PySide6.QtWidgets import QApplication, QDialog, QFileDialog
 from PySide6.QtCore import Qt
 from mainwindow import MainWindowUI
 from locowindow import LocoWindow
+from addlocodialog import AddLocoDialog
 
 class App(QApplication):
     def __init__ (self, args):
         super().__init__()
         
+# Windows with dialogs are ones that may lose focus to their dialogs
+# Add to this list to ensure their dialogs are kept on top
+# These can be mainwindows (such as LocoWindow) or dialogs with subdialogs (eg. AddLocoDialog)
+windows_with_dialogs = (LocoWindow, AddLocoDialog)
+# Dialogs that need to get raised
+dialog_types = (QDialog, QFileDialog)
     
 # We can connect to the QApplication's focusChanged signal
 # This allows us to handle focus changes across the entire app
 def handle_focus_change(old_focus, new_focus):
     # Check if the newly focused widget belongs to a LocoWindow
-    if new_focus and isinstance(new_focus.window(), LocoWindow):
+    if new_focus and isinstance(new_focus.window(), windows_with_dialogs):
+        #print (f"New focus {new_focus.window()}")
         # Find the active application-modal dialog
         for widget in QApplication.topLevelWidgets():
             # Check if the widget is a QDialog and is visible
-            if isinstance(widget, QDialog) and widget.isVisible():
+            if isinstance(widget, dialog_types) and widget.isVisible():
+                #print (f"Widget is {widget}")
                 # Check for ApplicationModal modality
                 if widget.windowModality() == Qt.ApplicationModal:
                     # Manually raise the dialog to the front
                     widget.raise_()
                     widget.activateWindow()
-                    break # We found and raised the dialog, so we can stop
+                    # The first dialog window is the last opened
+                    # after raising that stop
+                    break 
         
 # Create QApplication instance 
 app = App(sys.argv)
