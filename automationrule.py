ad697ced5a65a0adb70fb32670f3630b4a4995a6
Stewart Watkiss <penguintutor@users.noreply.github.com>
2025-11-02 23:09:00 +0000

    Update AutomationSequence and AutomationStep

--- automationrule.py
+++ automationrule.py
@@ -1,14 +1,45 @@
 
-
+from devicemodel import device_model
+from eventbus import event_bus
 
 # Automation rule determines the actions
+# Name is just a user friendly name for the step - does not need to be unique but helpful if it is
+# Eg. Activate point 1, or Set Loco 1 speed 3
+# Type is rule category - that could be
+# Loco, VLCB, App, Gui, Timer etc.
 # data is a dict with any additional parameters
 # eg. data['loco_id'] is used if the rule controls a loco
+# For all others then it needs to be all the parameters for the Event etc.
+# For loco_id - must have already been converted to the actual DCC ID
+# Done when the sequence is first activated (eg. request from user)
+# Flow is not included in the rule - that must be done at a higher level
 class AutomationRule:
+    
+# The event_map is taken from device_model.event_map
+#     event_map = {
+#         'VLCB': DeviceEvent,		# This should be used in preference to Device
+#         'Device': DeviceEvent,
+#         'Loco': LocoEvent,
+#         'App': AppEvent,
+#         'Gui': GuiEvent,
+#         'Timer': TimerEvent
+#         }
+    
+    
+    
     def __init__ (self, rule_name, rule_type, data):
         self.name = rule_name
         self.type = rule_type
         self.data = data
+        
+        # Create the corresponding event
+        self.event = device_model.event_map[self.type](self.data)
+        
+        
+    # Runs the action
+    def run (self):
+        # Assuming this is an event then broadcast to event_bus
+        event_bus.broadcast(self.event)
     
     def __repr__(self):
         return (f"Rule {self.rule_type}, {self.data}")
--- automationsequence.py
+++ automationsequence.py
@@ -1,17 +1,46 @@
 
 # Automation routine, composed of multiple steps
+# Each step is a rule, command or launch another sequence
+# These are provided as a list with each entry as a dict with the AutomationStep
 class AutomationSequence:
     def __init__(self, title, steps, settings = {}):
         self.title = title
         self.steps = steps  # List of AutomationStep objects
-        self.num_locos = settings['num_locos'] # 1, 2, or 3 locos required
+        self.settings = settings
+        if num_locos in settings:
+            self.num_locos = settings['num_locos'] # 1, 2, or 3 locos required
 
     def __repr__(self):
+        return f"AutomationSequence (title, steps, settings): {self.title} ({self.num_locos} Locos)"
+    
+
+    def __str__(self):
         return f"Sequence: {self.title} ({self.num_locos} Locos)"
     
     
     
-# Each step contains one or more rules, commands or sequences
+# Each step contains a rule commands or sequences
+# These are created from a dict and then extracted for the Automation Rule
 class AutomationStep:
-    def __init__(self, rules, settings={}):
-        self.rules = rules  # List of AutomationRule objects
+    # sequence is the sequence this is part of (needed for loops etc.)
+    # type is Rule, Var (plus operation), Label, Loop, Sequence
+    # name is the name passed to the rule, or in the case of the label is the actual label
+    # all other parameters are included in settings
+    def __init__(self, parent, step_type, step_name, settings={}):
+        self.parent = parent #parent sequence
+        self.step_type = data["type"]
+        self.step_name = data["name"]
+        self.data = data
+        
+        # If the step_type is a rule then create an automation rule
+        if self.step_type == "Rule":
+            self.rule = AutomationRule(step_name, step_type, data)
+            
+            
+    def run (self):
+        if self.step_type == "Rule":
+            self.rule.run()
+        
+        
+    def __repr__(self):
+        return f"Step: {self.step_type}: {self.step_name}"
